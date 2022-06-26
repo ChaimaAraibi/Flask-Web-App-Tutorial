e005ff3e1439f7f38608b3c9a0cb75aff48fe709
ChaimaAraibi <chaima.araibi@insat.u-carthage.tn>
2022-06-26 17:20:48 +0100

    added occupation

--- website/auth.py
+++ website/auth.py
@@ -40,6 +40,7 @@ def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
         first_name = request.form.get('firstName')
+        occupation = request.form.get('occupation')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
@@ -55,7 +56,7 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
+            new_user = User(email=email, first_name=first_name, occupation = occupation, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
